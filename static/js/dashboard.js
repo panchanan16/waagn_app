c1fff666842a47d119dbb67895c5a41e44ca7096
panchanan16 <dekapanchanan16@gmail.com>
2025-02-24 13:19:56 +0530

    dashboad re bug fixed

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,12 +4,12 @@ async function renderDashBoardData() {
     const response = await request.GET_POST('v1/dashboard/data', 'GET')
     if (response.success) {
         console.log(response)
-        document.getElementById('total-order').textContent = response.data[0][0].total_orders
-        document.getElementById('active-vehicles').textContent = response.data[1][0].active_vehicles
-        document.getElementById('delivered-order').textContent = response.data[0][0].total 
-        document.getElementById('transit-order').textContent = response.data[0][4].total 
-        document.getElementById('pending-pickup-order').textContent = response.data[0][3].total 
-        document.getElementById('pending_acceptance').textContent = response.data[2][0].pending_acceptance 
+        document.getElementById('total-order').textContent = response.data[0][0].total_orders 
+        document.getElementById('active-vehicles').textContent = response.data[1][0].active_vehicles ? response.data[1][0].active_vehicles : 0
+        document.getElementById('delivered-order').textContent = response.data[0][0].total ? response.data[0][0].total : 0
+        document.getElementById('pending-pickup-order').textContent = response.data[0][3].total ? response.data[0][3].total : 0
+        document.getElementById('transit-order').textContent = response.data[0][4].total ? response.data[0][4].total : 0 
+        document.getElementById('pending_acceptance').textContent = response.data[2][0].pending_acceptance  ? response.data[2][0].pending_acceptance : 0
     }
 }
 
