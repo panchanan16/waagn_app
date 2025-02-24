5b8b92953f1cbc731d4cc91495a72dbd420cf735
panchanan16 <dekapanchanan16@gmail.com>
2025-02-24 13:49:57 +0530

    dashboard re bug solved

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,11 +4,14 @@ async function renderDashBoardData() {
     const response = await request.GET_POST('v1/dashboard/data', 'GET')
     if (response.success) {
         console.log(response)
+
+        response.data[0].forEach((item)=> {
+            console.log(item.order_status)
+            const target = document.getElementById(item.order_status)
+            if (target) { target.textContent = item.total }           
+        })
+
         document.getElementById('total-order').textContent = response.data[0][0].total_orders 
-        document.getElementById('active-vehicles').textContent = response.data[1][0].active_vehicles ? response.data[1][0].active_vehicles : 0
-        document.getElementById('delivered-order').textContent = response.data[0][0].total ? response.data[0][0].total : 0
-        document.getElementById('pending-pickup-order').textContent = response.data[0][3].total ? response.data[0][3].total : 0
-        document.getElementById('transit-order').textContent = response.data[0][4].total ? response.data[0][4].total : 0 
         document.getElementById('pending_acceptance').textContent = response.data[2][0].pending_acceptance  ? response.data[2][0].pending_acceptance : 0
     }
 }
